3a8eebc07b0e67ea061fe355ca6b983ddee71e1f
Nail Khasipov <nail.khasipov@gmail.com>
2018-01-01 18:23:59 +0500

    pause game

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import {canvas, ctx} from './library/canvas';
 import {cursor} from './library/cursor';
+import {keyboard} from './library/keyboard';
 import {collisionDetection} from './library/collision';
 
 import Player from './Player';
 import Enemy from './Enemy';
+
+let pause = false;
+let SPACE = keyboard(32);
+SPACE.press = () => {pause = !pause};
 
 let player = new Player(10, 10, 0.5);
 let bullets = [];
@@ -15,19 +20,27 @@ canvas.addEventListener('mousedown', function(event) {
   bullets.push(player.shoot(target));
 });
 
-setInterval(function() {
-  // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-  let x = Math.floor(Math.random() * canvas.width) + 0;
-  let y = Math.floor(Math.random() * canvas.height) + 0;
-  let zombie = new Enemy(x, y, 0.1);
-  zombies.push(zombie);
-}, 1000);
+generateZombies();
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (!pause) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  cursor.draw();
+    cursor.draw();
+    drawBullets();
+    drawZombies();
+    collisionDetection(bullets, zombies);
 
+    player.move();
+    player.draw();
+  }
+
+  requestAnimationFrame(draw);
+}
+
+requestAnimationFrame(draw);
+
+function drawBullets() {
   bullets.forEach((bullet, index) => {
     if (bullet.x > 0 && bullet.x < canvas.width && bullet.y > 0 && bullet.y < canvas.width) {
       bullet.move();
@@ -36,18 +49,22 @@ function draw() {
       bullets.splice(index, 1);
     }
   });
+}
 
+
+function drawZombies() {
   zombies.forEach(zombie => {
     zombie.follow(player);
     zombie.draw();
   });
-
-  collisionDetection(bullets, zombies);
-
-  player.move();
-  player.draw();
-  
-  requestAnimationFrame(draw);
 }
 
-requestAnimationFrame(draw);
+function generateZombies() {
+  setInterval(function() {
+    // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+    let x = Math.floor(Math.random() * canvas.width) + 0;
+    let y = Math.floor(Math.random() * canvas.height) + 0;
+    let zombie = new Enemy(x, y, 0.1);
+    zombies.push(zombie);
+  }, 1000);
+}
