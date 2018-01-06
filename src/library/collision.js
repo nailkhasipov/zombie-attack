8fc5784b69ab9828ccc5d03b58fc4b3e32bf726a
Nail Khasipov <nail.khasipov@gmail.com>
2018-01-06 23:45:01 +0500

    remove comment

--- src/library/collision.js
+++ src/library/collision.js
@@ -1,5 +1,4 @@
 export function isCollide(bullet, zombie) {
-  // https://stackoverflow.com/questions/16792841/detect-if-user-clicks-inside-a-circle
   return Math.sqrt((zombie.x-bullet.x)*(zombie.x-bullet.x) +
                    (zombie.y-bullet.y)*(zombie.y-bullet.y)) < 5;
 }
